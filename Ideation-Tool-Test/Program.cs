54317ed318e9e00235e8b2ae4e76ee48ad9c376b
frederiksmits <frederiksmits@icloud.com>
2024-01-03 11:17:32 +0100

    Simple working Excel-Parser

--- Ideation-Tool-Test/Program.cs
+++ Ideation-Tool-Test/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello world");
+﻿using OfficeOpenXml;
+
+// Pad naar je Excel-bestand
+var filePath = @"/Applications/Ideation-Test.xlsx";
+
+// Zorg ervoor dat EPPlus de licentievoorwaarden accepteert.
+//ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+// Open het Excel-bestand
+using var package = new ExcelPackage(new FileInfo(filePath));
+
+// Kies het eerste werkblad
+var worksheet = package.Workbook.Worksheets[0];
+
+// Loop door de rijen en kolommen
+for (var row = 1; row <= worksheet.Dimension.End.Row; row++) 
+{ 
+    for (var col = 1; col <= worksheet.Dimension.End.Column; col++)
+    {
+        // Lees de waarde van de huidige cel
+        var value = worksheet.Cells[row, col].Text;
+
+        // Print de waarde in de console
+        Console.Write(value + "\t");
+    }
+    Console.WriteLine();
+}
